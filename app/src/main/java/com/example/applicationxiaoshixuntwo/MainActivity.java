9190fa2b9697c4c80df670dee0ecfefa25ddfc18
lxw526 <m13691097374_3@163.com>
2020-04-21 11:29:17 +0800

    添加222

--- app/src/main/java/com/example/applicationxiaoshixuntwo/MainActivity.java
+++ app/src/main/java/com/example/applicationxiaoshixuntwo/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        String s  = "111";
+        String s  = "222";
     }
 }
